f25914566d7e7df9aa9b7b8d9551837b1b1dd56e
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-04-27 13:23:59 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,41 @@
+function onPageLoaded() {
+    const images = document.querySelectorAll(".slider img");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextButton = document.getElementById("nextBtn");
+    const slider = document.getElementById("sliderid");
+
+    let count = 1;
+    let size = images[0].clientWidth;
+    slider.style.transform = 'translateX(' + (-size * count) + 'px)';
+
+    nextButton.addEventListener("click", function() {
+        if (count >= 8) return;
+        count++;
+        slider.style.transform = 'translateX(' + (-size * count) + 'px)';
+        slider.style.transition = "transform 1s ease-in-out";
+    });
+
+    prevBtn.onclick = function() {
+        if (count <= 0) return;
+        count--;
+        slider.style.transform = 'translateX(' + (-size * count) + 'px)';
+        slider.style.transition = "transform 1s ease-in-out";
+    };
+
+    slider.addEventListener("transitionend", () => {
+        if (images[count].id == "firstImg") {
+            slider.style.transition = "none";
+            count = 7;
+            slider.style.transform = 'translateX(' + (-size * count) + 'px)';
+        }
+
+        if (images[count].id == "lastImg") {
+            slider.style.transition = "none";
+            count = 1;
+            slider.style.transform = 'translateX(' + (-size * count) + 'px)';
+        }
+
+    })
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded);
